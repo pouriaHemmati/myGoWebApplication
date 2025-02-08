886421d308a0ea6d6bed9c47b4a3f341c0f3226c
pouria <pouria.he@gmail.com>
2025-02-09 00:45:49 +0330

    add lib chi

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,17 +1,21 @@
 package main
 
 import (
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"myGoWebApplication/pkg/config"
 	"myGoWebApplication/pkg/handlers"
 	"net/http"
 )
 
 func routes(c *config.AppConfig) http.Handler {
-	mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 
 	return mux
 }
